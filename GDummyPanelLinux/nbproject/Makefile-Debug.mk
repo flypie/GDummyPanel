33664b49a4a65b6ddfc802fe7982aa37aa6bf495
flypie <flypie@rocketmail.com>
2017-05-01 18:00:06 +0100

    Add Some Build projects for Netbeans GDummyPanelCygwin was built on Windows with Cygwin installed. While GDummyPanelLinux was built on Linux. My Main build is actuall Visual C, I will add ASAP, need to remove it from .Gitignore.
    Signed-off-by: flypie <flypie@rocketmail.com>

---
+++ GDummyPanelLinux/nbproject/Makefile-Debug.mk
@@ -0,0 +1,97 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-Linux
+CND_DLIB_EXT=so
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/_ext/5c0/Button.o \
+	${OBJECTDIR}/_ext/5c0/ComplexWindow.o \
+	${OBJECTDIR}/_ext/5c0/GDummyPanel.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=/usr/lib/libncurses.a -lpthread-2.23
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/gdummypanellinux
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/gdummypanellinux: /usr/lib/libncurses.a
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/gdummypanellinux: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/gdummypanellinux ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/_ext/5c0/Button.o: ../Button.cpp
+	${MKDIR} -p ${OBJECTDIR}/_ext/5c0
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/5c0/Button.o ../Button.cpp
+
+${OBJECTDIR}/_ext/5c0/ComplexWindow.o: ../ComplexWindow.cpp
+	${MKDIR} -p ${OBJECTDIR}/_ext/5c0
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/5c0/ComplexWindow.o ../ComplexWindow.cpp
+
+${OBJECTDIR}/_ext/5c0/GDummyPanel.o: ../GDummyPanel.cpp
+	${MKDIR} -p ${OBJECTDIR}/_ext/5c0
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/5c0/GDummyPanel.o ../GDummyPanel.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
